329bb451dde7dbd04b249459f0d381ce668258ef
deevo-sage <sidharthsahniii@gmail.com>
2021-05-11 00:06:56 +0530

    change client structure

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+function App() {
+  return (
+    <div
+      className="App"
+      style={{ background: "red", minHeight: "100vh", width: "100vw" }}
+    >
+      asdasdahuowadubuowi vbuowf buowfbuowf
+    </div>
+  );
+}
+
+export default App;
