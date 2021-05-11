1f4853f662c1564d170136a37d2d7f7d2e4d10f5
deevo-sage <sidharthsahniii@gmail.com>
2021-05-11 20:29:00 +0530

    changed config

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 export default {
-  db:
-    "mongodb+srv://adminuser123:pass123@cluster0.krhy9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  db: process.env.DATABASE,
 };
